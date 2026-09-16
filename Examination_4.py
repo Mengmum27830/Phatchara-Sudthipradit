c206eb6b38219d4a6d999a952b81bce3f2f0520b
Mengmum27830 <0830999129vivo@gmail.com>
2026-09-16 11:23:48 +0700

    Update Examination_4.py

--- Examination_4.py
+++ Examination_4.py
@@ -7,4 +7,15 @@
 
 
 # นักเรียนเขียนโค้ดต่อจากบรรทัดนี้
-score = int(input())
+# กรอกคะแนนสอบ
+score = int(input("คะแนนสอบ"))
+if score >= 80
+print("A")
+if score >= 70
+print ("B")
+if score >= 60
+print ("C")
+if score >= 50
+print ("D")
+else:
+print ("F")
